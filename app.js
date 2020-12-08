a42fcaabc8ba7588d10d3c6c7b56dffa882be8c5
Sebastian <57032919+valdresflya@users.noreply.github.com>
2020-12-08 22:38:12 +0100

    Further design and first login

--- app.js
+++ app.js
@@ -1,85 +1,123 @@
 const rec_albums = [
   {
     id: 1,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test1"
-  },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
   {
-    id: 3,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test3"
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b273d50051dd7fa4d034bb6636ac",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2737124b6bf785117033d003590",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b273b76f2e834dae7ba589354369",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b273745d59b647c30c5378510fe0",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b273a159f2b5ccc1faefea4aceac",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b273bdd53a2c8cb9730e9ef52cf3",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   },
-  { 
-    id: 2,
-    image:"https://i.scdn.co/image/ab67616d0000b273fb67a36bb6bcf4178cbaf643",
-    band:"Test2"
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
+  },
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
+  },
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
+  },
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
+  },
+  {
+    id: 1,
+    image:"https://i.scdn.co/image/ab67616d0000b2734ea56ef453ce4f457f6ebd42",
+    artist:"A band",
+    album:"An album"
   }
 ]
 
 
 Vue.component('card-comp', {
-  props: ['title','url'],
-  template: '<div class="col-12 col-sm-6 col-lg-4 col-xl-3"><div class="card mb-3 fade-in"><img class="card-img-top" :src="url" alt="Card image cap"><div class="card-body"><h5 class="card-title">{{title}}</h5></div></div></div>'
+  props: ['album','artist','url'],
+  template: `<div class="col-12 col-sm-6 col-lg-4 col-xl-3"><div class="card mb-3 fade-in album-card"><img class="card-img-top" :src="url" alt="Card image cap">
+  <div class="rating">97%</div>
+<div class="spotify-icons"><a href="#"><i class="fa fa-spotify fa-2x"></i></a></div>
+  <div class="album-text">
+<div class="album-name">{{album}}</div>
+<div class="artist-name">{{artist}}</div>`
 })
 
 albumVue = new Vue({
