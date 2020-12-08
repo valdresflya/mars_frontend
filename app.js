e3a6285894fec086304ddb292fce90ead80ea09e
Sebastian <57032919+valdresflya@users.noreply.github.com>
2020-12-08 16:32:22 +0100

    independent cards

---
+++ app.js
@@ -0,0 +1,6 @@
+var app = new Vue({
+    el: '#cardList',
+    data: {
+      cards: 'Hello Vue!'
+    }
+  })
--- fader.js
+++ fader.js
@@ -1,15 +1,27 @@
-$(window).scroll(function() {
-    $('.fade-in').each(function() {
-        var top_of_element = $(this).offset().top;
-        var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-        var bottom_of_screen = $(window).scrollTop() + $(window).innerHeight();
-        var top_of_screen = $(window).scrollTop();
+var $animation_elements = $('.card');
+var $window = $(window);
 
-        if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element) && !$(this).hasClass('is-visible')) {
-            $(this).addClass('is-visible');
-        }
-        if ((bottom_of_screen < top_of_element) && (top_of_screen < bottom_of_element) && $(this).hasClass('is-visible')) {
-            $(this).removeClass('is-visible');
-        }
+function check_if_in_view() {
+    console.log('something happening')
+    var window_height = $window.height();
+    var window_top_position = $window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+  
+    $.each($animation_elements, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+  
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= window_top_position) &&
+          (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
     });
-});
+  }
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
